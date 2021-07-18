9bb884b3ad4c4ae1dbc9c93a41b6682127106a77
Dilandk <dilan02007@gmail.com>
2021-07-18 22:39:53 +0200

    added all work files

---
+++ insideMaya.py
@@ -0,0 +1,163 @@
+import maya.OpenMaya as OpenMaya
+import json
+
+# Location where is picture saved
+picture_path = 'C:\Users\Dilan\Documents\GitHub\\from_2D_to_3D_with_python_maya'
+with open('C:\Users\Dilan\Documents\GitHub\\from_2D_to_3D_with_python_maya\\vertex_y_position.json') as json_file:
+    coordinates_data = json.load(json_file)
+
+
+def create_mesh(coordinates_data, faces, merge=True):
+    """
+    Given a list of vertices (iterables of floats) and a list of faces (iterable of integer vert indices),
+    creates and returns a maya Mesh
+
+    :param coordinates_data: dict of the face coordinates and current face vertices position
+    :type coordinates_data: dict
+    :param faces: list in the list of every 4 faces number ([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]])
+    :rtype faces: list in the list
+    :param merge: if we do want to merge vertices
+
+    :return: name of the created poly mesh
+    """
+    cmds.select(cl=True)
+    output_mesh = OpenMaya.MObject()
+
+    num_faces = len(faces)
+    num_vertices = len(coordinates_data) * 4
+
+    # Point array of plane vertex local positions
+    vertices_xyz_pos = OpenMaya.MFloatPointArray()
+
+    # Getting each vertices X Y Z position
+    for coord, vertex_pos in sorted(coordinates_data.items()):
+        for each_vt in vertex_pos:
+            p = OpenMaya.MFloatPoint(each_vt[0], each_vt[1] / 5, each_vt[2])
+            vertices_xyz_pos.append(p)
+
+    # Vertex connections per poly face in one array of index into point array given above
+    face_connects = OpenMaya.MIntArray()
+    for eachFace in faces:
+        # adding each face in to the Array
+        for eachCorner in eachFace:
+            face_connects.append(eachCorner)
+
+    # An array to hold the total number of vertices that each face has, one face do have 4 vertices
+    face_counts = OpenMaya.MIntArray()
+    for c in range(0, num_faces):
+        face_counts.append(4)
+
+    # Creating mesh object, using arrays above
+    mesh_fs = OpenMaya.MFnMesh()
+    mesh_fs.create(num_vertices, num_faces, vertices_xyz_pos, face_counts, face_connects, output_mesh)
+    # Getting name of new mesh
+    mesh_node_name = mesh_fs.name()
+    cmds.sets(mesh_node_name, add='initialShadingGroup')
+    cmds.select(mesh_node_name)
+    mesh_fs.updateSurface()
+    # This is useful because it deletes stray vertices (those not used in any faces)
+    if merge is True:
+        cmds.polyMergeVertex(mesh_node_name, ch=0)
+    mesh_fs.updateSurface()
+    return mesh_node_name
+
+
+def create_shader(name, node_type="lambert"):
+    """
+    Creating shader
+
+    :param name: prefix for the shader
+    :type name: str
+    :param node_type: maya shader name
+    :type node_type: str
+
+    :return: material and shader
+    :rtype: maya nodes
+    """
+    material = cmds.shadingNode(node_type, name='{name}_mat'.format(name=name), asShader=True)
+    sg = cmds.sets(name='{name}_SG'.format(name=name), empty=True, renderable=True, noSurfaceShader=True)
+    cmds.connectAttr('{mat}.outColor'.format(mat=material), '{sg}.surfaceShader'.format(sg=sg))
+    return material, sg
+
+
+def create_file_texture(name):
+    """
+    Creating file texture and place2dTexture. after connecting place2dTexture to the file texture
+
+    :param name: prefix for the file and place2dTexture node name
+    :type name: str
+
+    :return: file node name and place2dTexture node name
+    :rtype: maya nodes
+    """
+    texture_file = cmds.shadingNode('file', name='{name}_file'.format(name=name), asTexture=True, isColorManaged=True)
+    place2dTextureNode = cmds.shadingNode('place2dTexture',
+                                          name='{name}_place2dTexture'.format(name=name),
+                                          asUtility=True)
+
+    cmds.connectAttr('{p2d}.outUV'.format(p2d=place2dTextureNode), '{tex}.uvCoord'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.outUvFilterSize'.format(p2d=place2dTextureNode),
+                     '{tex}.uvFilterSize'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.vertexCameraOne'.format(p2d=place2dTextureNode),
+                     '{tex}.vertexCameraOne'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.vertexUvOne'.format(p2d=place2dTextureNode), '{tex}.vertexUvOne'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.vertexUvThree'.format(p2d=place2dTextureNode),
+                     '{tex}.vertexUvThree'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.vertexUvTwo'.format(p2d=place2dTextureNode), '{tex}.vertexUvTwo'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.coverage'.format(p2d=place2dTextureNode), '{tex}.coverage'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.mirrorU'.format(p2d=place2dTextureNode), '{tex}.mirrorU'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.mirrorV'.format(p2d=place2dTextureNode), '{tex}.mirrorV'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.noiseUV'.format(p2d=place2dTextureNode), '{tex}.noiseUV'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.offset'.format(p2d=place2dTextureNode), '{tex}.offset'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.repeatUV'.format(p2d=place2dTextureNode), '{tex}.repeatUV'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.rotateFrame'.format(p2d=place2dTextureNode), '{tex}.rotateFrame'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.rotateUV'.format(p2d=place2dTextureNode), '{tex}.rotateUV'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.stagger'.format(p2d=place2dTextureNode), '{tex}.stagger'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.translateFrame'.format(p2d=place2dTextureNode),
+                     '{tex}.translateFrame'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.wrapU'.format(p2d=place2dTextureNode), '{tex}.wrapU'.format(tex=texture_file))
+    cmds.connectAttr('{p2d}.wrapV'.format(p2d=place2dTextureNode), '{tex}.wrapV'.format(tex=texture_file))
+    return texture_file, place2dTextureNode
+
+
+faces = []
+# Creating dummy list to store one face
+dummy = []
+n = 0
+# For each 4 coordinates appending one face
+# Every 4 faces storing in the own list, it needed to be able to merge vertices later when we do create poly mesh
+for i in range(len(coordinates_data) * 4):
+    n += 1
+    dummy.append(i)
+    if n == 4:
+        faces.append(dummy)
+        dummy = []
+        n = 0
+
+# Creating poly mesh
+mesh_name = create_mesh(coordinates_data, faces)
+# Making UVs for poly mesh
+cmds.polyProjection('{name}.f[0:{total_faces}]'.format(name=mesh_name, total_faces=len(coordinates_data)),
+                    constructionHistory=True,
+                    type='Planar',
+                    insertBeforeDeformers=False,
+                    sf=True,
+                    md='b')
+
+# Assign shader to the mesh
+mtl, sg = create_shader(mesh_name)
+cmds.sets(mesh_name, edit=True, forceElement=sg)
+
+# Connecting file texture to the shader
+file_node, place2dTextureNode = create_file_texture(mesh_name)
+cmds.connectAttr('{node}.outColor'.format(node=file_node), '{node}.color'.format(node=mtl))
+
+# Sets Texture and rote texture
+cmds.setAttr('{node}.fileTextureName'.format(node=file_node),
+             '{path}\converted.png'.format(path=picture_path),
+             type='string')
+cmds.setAttr('{node}.rotateUV'.format(node=place2dTextureNode), 180)
+
+# Smooth model once
+cmds.polySmooth(mesh_name, dv=1)
+cmds.select(d=True)
---
+++ main.py
@@ -0,0 +1,173 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import json
+"""
+installed:
+    opencv-python module
+    Numpy-stl module
+    Pillow - module
+    matplotlib - module
+    json - module
+"""
+
+
+class Node:
+    """
+    Data structure that keeps track of face coordinates and current face vertices position
+    """
+    def __init__(self, xz, coordinates, next=None):
+        self.xz = xz
+        self.coordinates = coordinates
+        self.next = next
+
+
+class FaceLinkedList:
+    """
+    Class writing and reading information from the Node class
+    """
+    def __init__(self):
+        self.head = None
+
+    def write_vertices(self, xz, coordinates):
+        """
+        Writing face XZ position and vertices coordinates in to the Node class. After pointing to the previous Node
+
+        :param xz: face XZ position in maya space
+        :type xz: str
+        :param coordinates: Writing 4 vertices position  in maya space
+        :type: coordinates: list in the list
+        """
+        node = Node(xz, coordinates, self.head)
+        self.head = node
+
+    def get_vertices(self, xz):
+        """
+        Getting current face vertices coordinates in maya space
+
+        :param xz: face XZ coordinates in maya space
+        :type xz: str
+        :return: current face vertices coordinates in maya space
+        :rtype: list in the list
+        """
+        iterator = self.head
+        index = 0
+        while iterator:
+            if xz == iterator.xz:
+                return [iterator.coordinates]
+            iterator = iterator.next
+            index += 1
+
+
+# FaceLinkedList is used in the get_coord_from_img and in sorting dict which one will be saved in the file
+fll = FaceLinkedList()
+
+
+def get_coord_from_img(max_img_size, image_path, max_vertices_height):
+    """
+    1.  Load image from the file
+    2.  Convert image from color to grayscale
+    3.  Take pixel intensity in grayscale which goes from 0 to 55
+    4.  Create 3D object using the thickness of each coordinate in the picture
+    5.  Storing each vertices coordinates in the FaceLinkedList class,
+        each face (4 vertices) do have unique name (pixels coordinates X:Z)
+
+    :param max_img_size: image will be converted in to maximum size
+    :type max_img_size: tuple
+    :param image_path: image path
+    :type image_path: str
+    :param max_vertices_height: max vertices height which can reach on Y coordinates
+    :type image_path: int
+
+    :return: number of rows and columns
+    :rtype: int
+    """
+    # Converting image to grayscale
+    img = Image.open(image_path)
+    grey_img = img.convert('L')
+    rgb_img = img.convert('RGB')
+
+    # Resizing img if needed
+    grey_img.thumbnail(max_img_size)
+    rgb_img.thumbnail(max_img_size)
+
+    # Converting img to the array
+    image_np = np.array(grey_img)
+
+    # Getting Max and Min pixel Intensity
+    max_pix = image_np.max()
+
+    # nr_cols up/down, nr_rows - left/right
+    (nr_cols, nr_rows) = grey_img.size
+
+    # Logic of getting each pixel coordinates and storing each faces vertices
+    for z in range(0, nr_cols):
+        for x in range(0, nr_rows):
+            pixel_intensity = image_np[x][z]
+            # y is to height, so we making it smaller
+            y = (pixel_intensity * max_vertices_height) / max_pix
+            # Storing current 4 coordinates in the temp list, for easy reading and debugging
+            coordinates = []
+            coordinates.append([x, y, z])
+            coordinates.append([x-1, y, z])
+            coordinates.append([x-1, y, z + 1])
+            coordinates.append([x, y, z + 1])
+
+            # Storing coordinates of first row
+            if z == 0:
+                # To align two vertices with previous face (vertex number 2 and 3),
+                # We get previous face two vertices position and rewriting it in the current
+                if x != 0:
+                    new_coordinates = [coordinates[0],
+                                       fll.get_vertices('{x}:{z}'.format(x=x - 1, z=z))[0][0],
+                                       fll.get_vertices('{x}:{z}'.format(x=x - 1, z=z))[0][3],
+                                       coordinates[3]]
+                    fll.write_vertices('{x}:{z}'.format(x=x, z=z), new_coordinates)
+
+                # If it is the first column in the first row, we do save original vertices position
+                else:
+                    fll.write_vertices('{x}:{z}'.format(x=x, z=z), coordinates)
+
+            # Storing rest coordinates of the rows
+            else:
+                # First column
+                # To align two vertices with previous face (vertex number 1 and 2),
+                # We get previous face two vertices position and rewriting it in the current
+                if x == 0:
+                    new_coordinates = [fll.get_vertices('{x}:{z}'.format(x=x, z=z - 1))[0][3],
+                                       fll.get_vertices('{x}:{z}'.format(x=x, z=z - 1))[0][2],
+                                       coordinates[2],
+                                       coordinates[3]]
+                    fll.write_vertices('{x}:{z}'.format(x=x, z=z), new_coordinates)
+
+                # Rest columns
+                # To align two vertices with previous face (vertex number 1, 2, 3),
+                # We get previous face three vertices position and rewriting it in the current
+                elif x != 0:
+                    new_coordinates = [fll.get_vertices('{x}:{z}'.format(x=x, z=z - 1))[0][3],
+                                       fll.get_vertices('{x}:{z}'.format(x=x - 1, z=z))[0][0],
+                                       fll.get_vertices('{x}:{z}'.format(x=x - 1, z=z))[0][3],
+                                       coordinates[3]]
+                    fll.write_vertices('{x}:{z}'.format(x=x, z=z), new_coordinates)
+
+    # Flipping image and saving new image,
+    # This step needed for maya texture
+    out = rgb_img.transpose(Image.FLIP_LEFT_RIGHT)
+    out.save("converted.png", format="png")
+    return nr_cols, nr_rows
+
+
+# Getting number of columns, rows and storing all faces coordinates in the FaceLinkedList
+nr_cols, nr_rows = get_coord_from_img((300, 300), 'stadium.png', 80)
+
+# Writing all faces coordinates in to the dict, key is XZ faces pivot point coordinates,
+# Value is faces vertices coordinates
+all_vertices = {}
+for z in range(0, nr_cols):
+    for x in range(0, nr_rows):
+        coordinates = fll.get_vertices('{x}:{z}'.format(x=x, z=z))[0]
+        all_vertices['{x}:{z}'.format(x=x, z=z)] = coordinates
+
+# Writing all information in to the file
+with open('vertex_y_position.json', 'w') as f:
+    json.dump(all_vertices, f)
